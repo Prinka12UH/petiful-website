4c5502ebb7e0ff32b1928fdd5988a8dc1cac2c4c
Prinka12UH <132761662+Prinka12UH@users.noreply.github.com>
2023-05-22 15:15:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,183 @@
+(function ($) {
+
+  "use strict";
+
+  // Паралакс-ефект
+  $(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+  // Ефект накладання для зображень
+  var fullHeight = function () {
+    $('.js-fullheight').css('height', $(window).height());
+    $(window).resize(function () {
+      $('.js-fullheight').css('height', $(window).height());
+    });
+  };
+  fullHeight();
+
+  // Ефект завантажування сторінки
+  var loader = function () {
+    setTimeout(function () {
+      if ($('#ftco-loader').length > 0) {
+        $('#ftco-loader').removeClass('show');
+      }
+    }, 1);
+  };
+  loader();
+
+  // Панель відгуків та прокрутка
+  var carousel = function () {
+    $('.carousel-testimony').owlCarousel({
+      center: true,
+      loop: true,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: false,
+      navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+  };
+  carousel();
+
+  $('nav .dropdown').hover(function () {
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function () {
+    var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+  });
+
+  $('#dropdown04').on('show.bs.dropdown', function () {
+    console.log('show');
+  });
+
+  // Плавність спливаючих вікон
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  // iFrame для відео 
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  // Анімація чисел
+  var counter = function () {
+    $('#section-counter').waypoint(function (direction) {
+      if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+        var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+        $('.number').each(function () {
+          var $this = $(this),
+            num = $this.data('number');
+          console.log(num);
+          $this.animateNumber({
+            number: num,
+            numberStep: comma_separator_number_step
+          }, 7000);
+        });
+      }
+    }, {
+      offset: '95%'
+    });
+  }
+  counter();
+
+  // Анімація
+  var contentWayPoint = function () {
+    var i = 0;
+    $('.ftco-animate').waypoint(function (direction) {
+      if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+        i++;
+        $(this.element).addClass('item-animate');
+        setTimeout(function () {
+          $('body .ftco-animate.item-animate').each(function (k) {
+            var el = $(this);
+            setTimeout(function () {
+              var effect = el.data('animate-effect');
+              if (effect === 'fadeIn') {
+                el.addClass('fadeIn ftco-animated');
+              } else if (effect === 'fadeInLeft') {
+                el.addClass('fadeInLeft ftco-animated');
+              } else if (effect === 'fadeInRight') {
+                el.addClass('fadeInRight ftco-animated');
+              } else {
+                el.addClass('fadeInUp ftco-animated');
+              }
+              el.removeClass('item-animate');
+            }, k * 50, 'easeInOutExpo');
+          });
+        }, 100);
+      }
+    }, {
+      offset: '95%'
+    });
+  };
+  contentWayPoint();
+
+  // Вибір дати консультації
+  $('.appointment_date').datepicker({
+    'format': 'dd.mm.yyyy',
+    'autoclose': true,
+  });
+
+  // Вибір часу консультації
+  $('.appointment_time').timepicker();
+
+  // Налаштування вибору дати
+  $.fn.datepicker.dates['en'] = {
+    months: ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"],
+    monthsShort: ["Січ", "Лют", "Бер", "Квіт", "Трав", "Черв", "Лип", "Серп", "Вер", "Жовт", "Лист", "Груд"],
+  };
+
+  // Плавна прокрутка для якорів
+  var $page = $('html, body');
+  $('a[href*="#"]').click(function () {
+    $page.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+  });
+
+})(jQuery);
